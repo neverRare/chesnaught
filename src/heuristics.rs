e609047935c5f3c771b19788829b24d9f92fe0f0
neverRare <neverrare@proton.me>
2025-12-14 16:55:05 +0800

    reimplement Eq and Hash

--- src/heuristics.rs
+++ src/heuristics.rs
@@ -1,6 +1,7 @@
 use std::{
     cmp::Ordering,
     fmt::{self, Display, Formatter},
+    hash::{Hash, Hasher},
 };
 
 use crate::{board::Board, color::Color, end_state::EndState};
@@ -20,7 +21,7 @@ impl Display for Estimated {
         Ok(())
     }
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy)]
 pub enum Advantage {
     End(EndState),
     Estimated(Estimated),
@@ -45,6 +46,20 @@ impl Default for Advantage {
         Advantage::Estimated(Estimated::default())
     }
 }
+impl PartialEq for Advantage {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::End(a), Self::End(b)) => a == b,
+            (Self::Estimated(a), Self::Estimated(b)) => a == b,
+            (Self::End(EndState::Draw), Self::Estimated(advantage))
+            | (Self::Estimated(advantage), Self::End(EndState::Draw)) => {
+                &Estimated::default() == advantage
+            }
+            _ => false,
+        }
+    }
+}
+impl Eq for Advantage {}
 impl PartialOrd for Advantage {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
@@ -52,23 +67,53 @@ impl PartialOrd for Advantage {
 }
 impl Ord for Advantage {
     fn cmp(&self, other: &Self) -> Ordering {
+        macro_rules! draw {
+            () => {
+                Advantage::End(EndState::Draw)
+            };
+        }
+        macro_rules! white_wins {
+            () => {
+                Advantage::End(EndState::Win(Color::White))
+            };
+        }
+        macro_rules! black_wins {
+            () => {
+                Advantage::End(EndState::Win(Color::Black))
+            };
+        }
         match (self, other) {
             (Advantage::Estimated(a), Advantage::Estimated(b)) => Ord::cmp(a, b),
-            (&Advantage::DRAW, Advantage::Estimated(advantage)) => {
+            (draw!(), Advantage::Estimated(advantage)) => {
                 Ord::cmp(&Estimated::default(), advantage)
             }
-            (Advantage::Estimated(advantage), &Advantage::DRAW) => {
+            (Advantage::Estimated(advantage), draw!()) => {
                 Ord::cmp(advantage, &Estimated::default())
             }
 
-            (&Advantage::BLACK_WINS, &Advantage::BLACK_WINS)
-            | (&Advantage::WHITE_WINS, &Advantage::WHITE_WINS)
-            | (&Advantage::DRAW, &Advantage::DRAW) => Ordering::Equal,
+            (black_wins!(), black_wins!())
+            | (white_wins!(), white_wins!())
+            | (draw!(), draw!()) => Ordering::Equal,
 
-            (_, &Advantage::BLACK_WINS) | (&Advantage::WHITE_WINS, _) => Ordering::Greater,
+            (_, black_wins!()) | (white_wins!(), _) => Ordering::Greater,
 
-            (&Advantage::BLACK_WINS, _) | (_, &Advantage::WHITE_WINS) => Ordering::Less,
+            (black_wins!(), _) | (_, white_wins!()) => Ordering::Less,
         }
+    }
+}
+impl Hash for Advantage {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        #[derive(Hash)]
+        pub enum Hashable {
+            End(EndState),
+            Estimated(Estimated),
+        }
+        let hashable = match self {
+            Advantage::End(EndState::Draw) => Hashable::Estimated(Estimated::default()),
+            Advantage::End(end_state) => Hashable::End(*end_state),
+            Advantage::Estimated(estimated) => Hashable::Estimated(*estimated),
+        };
+        hashable.hash(state);
     }
 }
 pub fn estimate(board: &Board) -> Estimated {
